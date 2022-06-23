45076173120cfc278bc6b115d1bf89e53b169e55
HammadAhmedSherazi <ahmedhammad262@gmail.com>
2022-06-23 21:18:08 +0500

    Project

--- index.php
+++ index.php
@@ -53,37 +53,9 @@ hr{
   <button onclick="topFunction()" id="myBtn" class="btn btn-outline-light" title="Go to top"><i class="fa-solid fa-angle-up"></i></button>
     <div class="container-fluid">
         <!-- Navigation Row -->
-        <div class="row">
-            <nav class="navbar navbar-expand-lg navbar-light col-12 fixed-top " style="background-color: rgb(55, 194, 232) ; box-shadow: rgba(247, 250, 253, 0.04) 0px px 0px, rgba(255, 255, 255, 0.25) 0px 3px 0px inset;">
-                <a class="navbar-brand" href="#">
-                    <img src="./images/LogoMakr-9eSAAC.png" alt="logo" width="40px" >
-                    <span style="color: white;">Filter&nbsp;</span><span>Finder</span><sup><strong>pk</strong></sup>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end  "  id="navbarNav">
-                    
-                  <ul class="navbar-nav">
-                    <li class="nav-item active mx-3">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item mx-3">
-                      <a class="nav-link" href="#contact">Contact</a>
-                    </li>
-                    <li class="nav-item mx-3">
-                      <a class="nav-link" href="#About">About</a>
-                    </li>
-                    <li class="nav-item mx-3">
-                      <a href="./RegForm.html" target="_blank">
-                        <button type="button" class="btn btn-outline-light" >Register Filter</button>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-              
-        </div>
+      <?php
+        include(".Header.php");
+      ?>
         <!-- Navigation Row End -->
 
         <!-- Slider Row  -->
